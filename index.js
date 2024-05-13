ec68675b7dd3c649500d87641089c6abc8d35ca5
Mario Castro <mcast032@gmail.com>
2024-05-12 17:00:38 -0700

    objects lab finished

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+    name: "Mario",
+    street: "401 s. Hobart"
+
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const newObj = { ...object };
+    newObj[key] = value;
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    employee[key] = value;
+    
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    
+    const newEmployee = { ...employee };
+    
+    delete newEmployee[key];
+    
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   
+    delete employee[key];
+  
+    return employee;
+}
